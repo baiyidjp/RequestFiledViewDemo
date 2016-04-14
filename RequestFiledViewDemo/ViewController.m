139a05970503799972ace7489cbdb63405ac1736
baiyidjp <tztddong@dongjiangpengdeMac-mini.local>
2016-04-14 13:30:21 +0800

    增加demo

--- RequestFiledViewDemo/ViewController.m
+++ RequestFiledViewDemo/ViewController.m
@@ -22,10 +22,12 @@
     
     self.title = @"主页";
 }
+
+#pragma mark 无内容
 //使用默认的提示title和图片
 - (IBAction)one:(id)sender {
     
-    _requestView = [RequestFiledView configViewWithFrame:self.view.bounds Type:2 selectBlock:^{
+    _requestView = [RequestFiledView configViewWithFrame:self.view.bounds Type:1 selectBlock:^{
 
             NSLog(@"刷新");
             if (_requestView) {
@@ -40,7 +42,7 @@
 //自定义提示title 使用默认图片
 - (IBAction)two:(id)sender {
     
-    _requestView = [RequestFiledView configViewWithFrame:self.view.bounds Title:@"自定义文字" Type:2 selectBlock:^{
+    _requestView = [RequestFiledView configViewWithFrame:self.view.bounds Title:@"自定义文字" Type:1 selectBlock:^{
             NSLog(@"刷新");
             if (_requestView) {
                 
@@ -53,7 +55,7 @@
 //使用默认title 自定义图片
 - (IBAction)three:(id)sender {
     
-    _requestView = [RequestFiledView configViewWithFrame:self.view.bounds ImageName:@"wuwang_wuwangluotubiao" Type:2 selectBlock:^{
+    _requestView = [RequestFiledView configViewWithFrame:self.view.bounds ImageName:@"wuwang_wuwangluotubiao" Type:1 selectBlock:^{
         NSLog(@"刷新");
         if (_requestView) {
             
@@ -66,6 +68,60 @@
 }
 //自定义提示title 自定义图片
 - (IBAction)four:(id)sender {
+    
+    _requestView = [RequestFiledView configViewWithFrame:self.view.bounds Title:@"自定义两个" ImageName:@"wuwang_wuwangluotubiao" Type:1 selectBlock:^{
+        NSLog(@"刷新");
+        if (_requestView) {
+            
+            [_requestView removeFromSuperview];
+            _requestView = nil;
+        }
+    }];
+    [self.view addSubview:_requestView];
+}
+
+#pragma mark 无网络
+
+- (IBAction)oneone:(id)sender {
+    
+    _requestView = [RequestFiledView configViewWithFrame:self.view.bounds Type:2 selectBlock:^{
+        
+        NSLog(@"刷新");
+        if (_requestView) {
+            
+            [_requestView removeFromSuperview];
+            _requestView = nil;
+        }
+    }];
+    
+    [self.view addSubview:_requestView];
+}
+- (IBAction)twotwo:(id)sender {
+    
+    _requestView = [RequestFiledView configViewWithFrame:self.view.bounds Title:@"自定义文字" Type:2 selectBlock:^{
+        NSLog(@"刷新");
+        if (_requestView) {
+            
+            [_requestView removeFromSuperview];
+            _requestView = nil;
+        }
+    }];
+    [self.view addSubview:_requestView];
+}
+- (IBAction)threethree:(id)sender {
+    
+    _requestView = [RequestFiledView configViewWithFrame:self.view.bounds ImageName:@"wuwang_wuwangluotubiao" Type:2 selectBlock:^{
+        NSLog(@"刷新");
+        if (_requestView) {
+            
+            [_requestView removeFromSuperview];
+            _requestView = nil;
+        }
+    }];
+    [self.view addSubview:_requestView];
+
+}
+- (IBAction)fourfour:(id)sender {
     
     _requestView = [RequestFiledView configViewWithFrame:self.view.bounds Title:@"自定义两个" ImageName:@"wuwang_wuwangluotubiao" Type:2 selectBlock:^{
         NSLog(@"刷新");
